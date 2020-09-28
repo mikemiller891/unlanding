add379d09f714aa20e9103dafa858475a2f1f8cf
Mike Miller <mike@reslar.com>
2020-09-28 12:37:31 -0500

    Only change the background weekly.

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $_SERVER['HTTP_HOST'] ?></title>
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?= $_SERVER['HTTP_HOST'] ?></title>
+  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
 <body class="h-screen w-screen flex flex-col justify-center bg-indigo-200 bg-cover bg-center bg-fixed"
-  style="background-image:url(https://source.unsplash.com/featured/?desk)">
+  style="background-image:url(https://source.unsplash.com/weekly/?desk)">
 
 <div class="flex justify-center">
   <div class="bg-white py-2 px-16 opacity-50 shadow-2xl rounded-full">
