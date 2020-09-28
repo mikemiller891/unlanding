655c9ee2da12bc7913a3ed2d86623e771c854974
Mike Miller <66344776+mikemiller891@users.noreply.github.com>
2020-09-28 11:27:27 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $_SERVER['HTTP_HOST'] ?></title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<body class="h-screen w-screen flex flex-col justify-center bg-indigo-200 bg-cover bg-center bg-fixed"
+  style="background-image:url(https://source.unsplash.com/featured/?desk)">
+
+<div class="flex justify-center">
+  <div class="bg-white py-2 px-16 opacity-50 shadow-2xl rounded-full">
+    <h1 class="text-6xl text-center font-bold"><?= $_SERVER['HTTP_HOST'] ?></h1>
+  </div>
+</div>
+
+</body>
+
+</html>
