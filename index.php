8896f727de57b6d7ae87ad95dffbb31cb46ff686
Mike Miller <mike@reslar.com>
2020-09-29 01:08:02 -0500

    Removed dependency on Tailwind CSS.

--- index.php
+++ index.php
@@ -5,19 +5,46 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title><?= $_SERVER['HTTP_HOST'] ?></title>
-  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+  <title><?= $_SERVER['SERVER_NAME'] ?></title>
+  <style>
+    body {
+      margin: 0;
+      height: 100vh;
+      width: 100vw;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      background-color: rgba(195, 218, 254, 1);
+      background-size: cover;
+      background-position: center;
+      background-attachment: fixed;
+      background-image:url(https://source.unsplash.com/daily/?coffee,desk,sunrise);
+    }
+    div {
+      display: flex;
+      justify-content: center;
+    }
+    div div {
+      background-color: rgba(195, 218, 254, 0.5);
+      box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 1);
+      border-radius: 9999px;
+      padding-top: 0.5rem;
+      padding-bottom: 0.5rem;
+      padding-left: 2rem;
+      padding-right: 2rem;
+      font-size: 4rem;
+      font-weight: 700;
+      font-family: sans-serif;
+      color: rgba(0, 0, 0, 1);
+      opacity: 1;
+    }
+  </style>
 </head>
 
-<body class="h-screen w-screen flex flex-col justify-center bg-indigo-200 bg-cover bg-center bg-fixed"
-  style="background-image:url(https://source.unsplash.com/weekly/?desk)">
-
-<div class="flex justify-center">
-  <div class="bg-white py-2 px-16 opacity-50 shadow-2xl rounded-full">
-    <h1 class="text-6xl text-center font-bold"><?= $_SERVER['HTTP_HOST'] ?></h1>
+<body>
+  <div>
+    <div><?= $_SERVER['SERVER_NAME'] ?></div>
   </div>
-</div>
-
 </body>
 
 </html>
